b19a033c839c9d9cec73b06a060157a43684c445
Jonathan Robinson Manchego Sosa <jonathan571999@gmail.com>
2022-02-04 08:56:35 -0500

    EVALUANDO CONOCIMIENTOS

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAT0qQu7hKM-2aCfvn_K0xWOMxeyVxqrlc",
+    authDomain: "clientapptechtest.firebaseapp.com",
+    projectId: "clientapptechtest",
+    storageBucket: "clientapptechtest.appspot.com",
+    messagingSenderId: "895279699115",
+    appId: "1:895279699115:web:be4bafdce2fa8f187113ee",
+    measurementId: "G-EDZHBZS6E4"
+  }
 };
